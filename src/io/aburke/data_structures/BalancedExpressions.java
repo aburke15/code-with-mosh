8efaea0b21ad10013ee8404d4dccf20b48bee60b
André Burke <aburke@Andres-MacBook-Pro.local>
2020-07-01 20:59:01 -0600

    added start of balanced expressions for stacks

---
+++ src/io/aburke/data_structures/BalancedExpressions.java
@@ -0,0 +1,14 @@
+package io.aburke.data_structures;
+
+public class BalancedExpressions {
+    public boolean IsBalanced(String expression) {
+        if (expression == null)
+            throw new IllegalStateException();
+            
+        for (var ch : expression.toCharArray()) {
+            
+        }
+
+        return false;
+    }
+}
