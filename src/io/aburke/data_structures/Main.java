aaf2a4c9de8510712e214d45d74626b9944fa193
André Burke <aburke@Andres-MacBook-Pro.local>
2020-07-13 22:44:29 -0600

    added priority queue implementation

--- src/io/aburke/data_structures/Main.java
+++ src/io/aburke/data_structures/Main.java
@@ -7,9 +7,19 @@ public class Main {
         var queue = new PriorityQueue();
 
         queue.enqueue(1);
-        queue.enqueue(3);
         queue.enqueue(5);
+        queue.enqueue(3);
         queue.enqueue(7);
         queue.enqueue(2);
+        queue.dequeue();
+        queue.enqueue(6);
+        queue.enqueue(8);
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
     }
 }
--- src/io/aburke/data_structures/PriorityQueue.java
+++ src/io/aburke/data_structures/PriorityQueue.java
@@ -4,22 +4,65 @@ import java.util.Arrays;
 
 public class PriorityQueue {
     private int[] items;
-    private int size, first, last;
+    private int size;
 
     public PriorityQueue() {
         items = new int[2];
-        size = first = last = 0;
+        size = 0;
     }
 
     public void enqueue(int item) {
         if (isFull())
             resize(items.length * 2);
 
+        if (size == 0 || item > items[size - 1]) {
+            items[size++] = item;
+            return;
+        }
+
+        shiftItemsToInsert(item);
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var item = items[--size];
+        items[size] = 0;
+
+        if (size > 0 && size == items.length / 4)
+            resize(items.length / 2);
+
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return items[size - 1];
+    }
+
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    private boolean isFull() {
+        return size >= items.length;
+    }
+
+    private void reverseItemsInQueue(int k) {
+        
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+
+    private void shiftItemsToInsert(int item) {
         for (var i = size == 0 ? 0 : size - 1; i >= 0; i--) {
-            if (size == 0 || item > items[size - 1]) {
-                items[size++] = item;
-                return;
-            } else if (item < items[i]) {
+            if (item < items[i]) {
                 items[i + 1] = items[i];
                 items[i] = item;
             }
@@ -28,38 +71,11 @@ public class PriorityQueue {
         size++;
     }
 
-    public int dequeue() {
-        if (isEmpty())
-            throw new IllegalStateException();
-
-        return items[--size];
-    }
-
-    public int peek() {
-        if (isEmpty())
-            throw new IllegalStateException();
-
-        return items[first == 0 ? 0 : first - 1];
-    }
-
-    public boolean isEmpty() {
-        return size <= 0;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(items);
-    }
-
     private void resize(int length) {
         int[] temp = new int[length];
-        for (var i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) 
             temp[i] = items[i];
 
         items = temp;
-    }
-
-    private boolean isFull() {
-        return size >= items.length;
     }
 }
