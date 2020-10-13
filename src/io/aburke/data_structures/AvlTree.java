c66ff7160633f1a54d611a1fdce60bf900468c76
Andre Burke <aburkex24@gmail.com>
2020-10-12 23:48:23 -0600

    added insert with more concise implementation

--- src/io/aburke/data_structures/AvlTree.java
+++ src/io/aburke/data_structures/AvlTree.java
@@ -9,6 +9,11 @@ public class AvlTree {
         public AvlNode(int value) {
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
     }
 
     private int size;
@@ -19,37 +24,55 @@ public class AvlTree {
     }
 
     public void insert(int value) {
-        var node = new AvlNode(value);
-
-        if (root == null) {
-            root = node;
-            return;
-        }
-
-        insert(root, node);
+        this.root = insert(this.root, value);
     }
 
-    private void insert(AvlNode root, AvlNode child) {
-        var rootValue = root.value;
-        var childValue = child.value;
+    private AvlNode insert(AvlNode root, int value) {
+        if (root == null)
+            return new AvlNode(value);
 
-        if (childValue < rootValue) {
-            if (root.leftChild == null) {
-                root.leftChild = child;
-                return;
-            }
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else if (value > root.value)
+            root.rightChild = insert(root.rightChild, value);
 
-            insert(root.leftChild, child);
-        }
-
-        if (childValue > rootValue) {
-            if (root.rightChild == null) {
-                root.rightChild = child;
-                return;
-            }
-
-            insert(root.rightChild, child);
-        }
-
+        return root;
     }
+
+/// My initial implementation works but it's not concise
+//    public void insert(int value) {
+//        var node = new AvlNode(value);
+//
+//        if (root == null) {
+//            root = node;
+//            return;
+//        }
+//
+//        insert(root, node);
+//    }
+//
+//    private void insert(AvlNode root, AvlNode child) {
+//        var rootValue = root.value;
+//        var childValue = child.value;
+//
+//        if (childValue < rootValue) {
+//            if (root.leftChild == null) {
+//                this.size++;
+//                root.leftChild = child;
+//                return;
+//            }
+//
+//            insert(root.leftChild, child);
+//        }
+//
+//        if (childValue > rootValue) {
+//            if (root.rightChild == null) {
+//                this.size++;
+//                root.rightChild = child;
+//                return;
+//            }
+//
+//            insert(root.rightChild, child);
+//        }
+//    }
 }
